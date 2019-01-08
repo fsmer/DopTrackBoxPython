702f6c4482287e1b3f7dfff92c9bb4ca819badf0
mc.bekkering@gmail.com <mc.bekkering@gmail.com>
2019-01-08 18:51:24 +0100

    Loopallsats output

--- LoopallSats.py
+++ LoopallSats.py
@@ -17,7 +17,7 @@ def LoopallSats(mode, line0, line1,line2,Rstation, PosStation, satelliteindex):
     timevector = []
     #Recalculate SGP4 and Continue
     k = 0
-    for j in range (0, len(line0)-1):
+    for j in range (0, len(line0)):
         Carthesian = (SGP4(line1[j], line2[j], year[k], month[k], day[k], hour[k], minute[k], second[k]))
         time = (year[k], month[k], day[k], hour[k], minute[k], second[k])
 
@@ -39,7 +39,7 @@ def LoopallSats(mode, line0, line1,line2,Rstation, PosStation, satelliteindex):
     #make yml file
     # makeyamlfile()
     #plot all sats on map
-    # PlotOnMap(latitudevector, longitudevector, timevector, inviewvector, PosStation, 0, mode)
+    PlotOnMap(latitudevector, longitudevector, timevector, inviewvector, PosStation, 0, mode, satelliteindex, line0)
     print("done")
     #plot view of sky
     #plotinsight
--- PlotOnMap.py
+++ PlotOnMap.py
@@ -1,42 +1,91 @@
 
-def PlotOnMap(Latvector,Lonvector, time, insight, station, minback, mode):
+def PlotOnMap(Latvector,Lonvector, time, insight, station, minback, mode, satelliteindex, satellites):
 
     import matplotlib.pyplot as plt
     import numpy as np
-    import datetime
+    import datetime 
 
+
+    if (mode == 0):
+        # mode 0 is all sats
+        # we need to plot all satellites locations and give their names
+        # maybe the satellite selected can be an other marker
+
+        img = plt.imread("map.png")
+        # implot = plt.imshow(im)
+
+        fig1, ax = plt.subplots()
+        ax.imshow(img, extent=[-180, 180, -90, 90])
+        ax.set_aspect(1)
+
+
+   
+        plt.plot(Lonvector, Latvector, 'b.', label='Satellites') 
+        plt.plot(station[0],station[1],'r+', label='Groundstation')
+     
+        plt.plot(Lonvector[satelliteindex], Latvector[satelliteindex],'rx', label=satellites[satelliteindex])
+
+        ax.legend(bbox_to_anchor=(0., 1.02, 1., .102),  ncol = 1)
+
+        plt.xlabel('Lontitude')
+        plt.ylabel('Latitude')
+        plt.text(90, 100, 'Satellites in database',
+            horizontalalignment='center')
+
+        # x = Lonvector
+        # plt.xticks(x, satellites)
+        # plt.xticks(rotation=90)
+        # y = Latvector
+        # plt.yticks(y, satellites)
+        
+        # plt.subplots_adjust(left=0.12, right=0.13, top=0.26, bottom=0.25)
+
+        for i, txt in enumerate(satellites):
+            ax.annotate(txt, (Lonvector[i], Latvector[i]), size = 7)
+        
+        #this makes the plot screen size
+        mng = plt.get_current_fig_manager()
+        mng.resize(*mng.window.maxsize())
+
+        plt.show(block=False)
+
+
+    elif (mode == 1):
+        print("Visual for loop x sats is not yet ready")
+
+    elif (mode == 3):
+        print("Visual for life updating is not yet ready")
+
+    # img = plt.imread("map.png")
+    # # implot = plt.imshow(im)
+
+    # fig1, ax = plt.subplots()
+    # ax.imshow(img, extent=[-180, 180, -90, 90])
+    # ax.set_aspect(1)
+
+
+
+    # # print(Lonvector[0][0][0])
+
+    # plt.plot(Lonvector[0][0], Latvector[0][0], 'b.') 
+    # plt.plot(station[0],station[1],'r+')
+    # plt.plot(Lonvector[0][0][minback], Latvector[0][0][minback],'rx')
+
+    # plt.xlabel('Lontitude')
+    # plt.ylabel('Latitude')
+    # plt.title('Satellite or groundtrack plot')
+
+
+
+    # fig2, ax2 = plt.subplots()
+    # x = np.linspace(1,len(insight))
+    # plt.plot(insight, '.')
+    # # ax2.set_aspect(2)
+
+    # plt.xticks(x, time)
+    # plt.xticks(rotation=90)
 
     
-    img = plt.imread("map.png")
-    # implot = plt.imshow(im)
-
-    fig1, ax = plt.subplots()
-    ax.imshow(img, extent=[-180, 180, -90, 90])
-    ax.set_aspect(1)
-
-
-
-    # print(Lonvector[0][0][0])
-
-    plt.plot(Lonvector[0][0], Latvector[0][0], 'b.') 
-    plt.plot(station[0],station[1],'r+')
-    plt.plot(Lonvector[0][0][minback], Latvector[0][0][minback],'rx')
-
-    plt.xlabel('Lontitude')
-    plt.ylabel('Latitude')
-    plt.title('Satellite or groundtrack plot')
-
-
-
-    fig2, ax2 = plt.subplots()
-    x = np.linspace(1,len(insight))
-    plt.plot(insight, '.')
-    # ax2.set_aspect(2)
-
-    plt.xticks(x, time)
-    plt.xticks(rotation=90)
-
-    
-    plt.show(block=False)
+    # plt.show(block=False)
     # print('hello')
     return
